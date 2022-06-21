3581d87ce5744d5d306b370d6e7e9f558c659735
Eugênio Júnior <eugeniojunior21@gmail.com>
2022-06-21 15:44:20 -0300

    js

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+var ola = String(document.getElementsByClassName("ola1")[0].innerHTML);
+    console.log(ola)
+
+var sobreButton = document.getElementById("sobreButton")[0].innerHTML;
+    console.log(sobreButton)
+
+

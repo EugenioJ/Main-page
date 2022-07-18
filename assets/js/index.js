f2db124adf0508fda3162e9a1ea4ac555641b1fa
Eugênio Júnior <eugeniojunior21@gmail.com>
2022-07-18 19:59:36 -0300

    remove bootstrap

--- assets/js/index.js
+++
@@ -1,7 +0,0 @@
-var ola = String(document.getElementsByClassName("ola1")[0].innerHTML);
-    console.log(ola)
-
-var sobreButton = document.getElementById("sobreButton")[0].innerHTML;
-    console.log(sobreButton)
-
-
